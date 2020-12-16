c68961eccc31b2f6a94d3a2cf44a1fa8449db3c6
jeeehwe <jeeehwe@gmail.com>
2020-12-16 15:42:18 +0900

    readme 추가

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -8,7 +8,7 @@ Vue.use(Router);
 export default new Router({
     mode: 'history',
     routes: [
-        { path: '/', component: Index.Home },
+        { path: '/', redirect: '/list/request' },
         { path: '/p/:payUrl', component: Index.PayUrl },
         { path: '/list/:state', component: Index.PaymentList },
     ],
